111aa3f80f0d11d00ff5e296c4e20cdafe404834
William Villeneuve <ciiqr@users.noreply.github.com>
2018-01-04 00:54:50 -0500

    I don't even understand the GitHub windows client...

--- install.ps1
+++ install.ps1
@@ -12,17 +12,58 @@ $install = Join-Path $workstation 'install.ps1'
 $saltDir = Join-Path $env:SystemDrive 'salt'
 $saltCall = Join-Path $saltDir 'salt-call'
 
+# functions
+function CustomTempDirectory {
+    # NOTE: I would rather generate a unique directory name and then delete it when done,
+    # but windows is stupid with the permissions of the newly downloaded file
+
+    $parent = [System.IO.Path]::GetTempPath()
+    $tmp = Join-Path $parent 'workstation'
+    if (![System.IO.Directory]::Exists($tmp)) {
+        [System.IO.Directory]::CreateDirectory($tmp)
+    }
+    return $tmp
+}
+
+function TryRemoveDirectory($dir) {
+    if ([System.IO.Directory]::Exists($dir)) {
+        Remove-Item $dir -Force -Recurse
+    }
+}
+
+# create temp directory
+echo 'Creating temp directory...'
+$tmp = CustomTempDirectory
+
+# temp files
+$saltFile = Join-Path $tmp $saltFilename
+$repoTempFile = Join-Path $tmp $repoFilename
+$repoTempDir = Join-Path $tmp 'workstation'
+$repoSubDir = Join-Path $repoTempDir $repoZipSubDir
 
 
+# download workstation
+echo 'Downloading workstation...'
+$down = New-Object System.Net.WebClient
+$down.DownloadFile($repoUrl, $repoTempFile)
+
+# unzip workstation
+TryRemoveDirectory $repoTempDir
+Expand-Archive $repoTempFile -DestinationPath $repoTempDir
+
+# move to system drive
+TryRemoveDirectory $workstation
+Move-Item $repoSubDir $workstation
 
 
+# download salt
+echo 'Downloading salt...'
+$down = New-Object System.Net.WebClient
+$down.DownloadFile($saltUrl, $saltFile)
 
-
-
-
-
-
-
+# install salt
+Set-ExecutionPolicy Bypass -Scope Process -Force
+& $saltFile /S /minion-name=workstation /start-minion=0
 
 
 # TODO: run salt highstate
