fb2ce2267367836f7d7057b90c9f51a60f01b477
William Villeneuve <ciiqr@users.noreply.github.com>
2018-01-06 10:30:04 -0500

    perform a choco upgrade after done salt provisioning (temporary solution for lack of pkg.latest equivalent)

--- scripts/provision.ps1
+++ scripts/provision.ps1
@@ -9,3 +9,6 @@ $saltCall = Join-Path $salt 'salt-call'
 # run salt highstate
 & $saltCall saltutil.sync_all
 & $saltCall --retcode-passthrough --state-verbose=False -l warning state.apply
+
+# TODO: unfortunately salt doesn't support the idea of 'pkg.latest' with choco... (I should write my own, but for now, this is fine)
+choco upgrade all -y
