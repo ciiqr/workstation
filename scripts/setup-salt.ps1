1972cf353f4683288420c94a05861d35859fb9e1
William Villeneuve <ciiqr@users.noreply.github.com>
2018-01-08 19:32:18 -0500

    renamed grains-windows to grains-windows.yaml

--- scripts/setup-salt.ps1
+++ scripts/setup-salt.ps1
@@ -14,7 +14,7 @@ function CreateSymlink($target, $link) {
 }
 
 # link configs
-CreateSymlink "$workstation\salt\etc\grains-windows" "$salt\conf\grains"
+CreateSymlink "$workstation\salt\etc\grains-windows.yaml" "$salt\conf\grains"
 CreateSymlink "$workstation\salt\etc\minion.conf" "$salt\conf\minion"
 CreateSymlink "$workstation\salt\etc\masterlike.conf" "$salt\conf\masterlike.conf"
 CreateSymlink "$workstation\salt\etc\masterless.conf" "$salt\conf\minion.d\masterless.conf"
