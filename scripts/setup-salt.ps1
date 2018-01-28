aa13ff3e102de74119c86b879fb6001532531e28
William Villeneuve <ciiqr@users.noreply.github.com>
2018-01-28 16:36:19 -0500

    calling salt-call directly, moved script functions to a common file

--- scripts/setup-salt.ps1
+++ scripts/setup-salt.ps1
@@ -1,17 +1,15 @@
 Param (
-    [Alias("SaltDir")] 
+    [Alias("SaltDir")]
     [string]$salt = $( Join-Path $env:SystemDrive 'salt' ),
-    [Alias("WorkstationDir")] 
+    [Alias("WorkstationDir")]
     [string]$workstation = $( Join-Path $env:SystemDrive 'workstation' )
 )
 
-# declarations
-$saltCall = Join-Path $salt 'salt-call'
-
 # functions
-function CreateSymlink($target, $link) {
-    New-Item -Path $link -ItemType SymbolicLink -Value $target -Force > $null
-}
+. "$PSScriptRoot\include\common.ps1"
+
+# create config directory
+mkdir -Force "$salt\conf\minion.d"
 
 # link configs
 CreateSymlink "$workstation\salt\etc\grains-windows.yaml" "$salt\conf\grains"
@@ -20,4 +18,4 @@ CreateSymlink "$workstation\salt\etc\masterlike.conf" "$salt\conf\masterlike.con
 CreateSymlink "$workstation\salt\etc\masterless.conf" "$salt\conf\minion.d\masterless.conf"
 
 # apply bootstrap state (for installing package managers and such)
-& $saltCall state.apply bootstrap
+salt-call state.apply bootstrap
